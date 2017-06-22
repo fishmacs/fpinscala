cc8ba99729936ec122f99d128eec71f905e4b65a
zw <wzhao1984@gmail.com>
2017-06-22 23:15:33 +0800

    exercise 4.6

--- src/main/scala/chap04/exer.scala
+++ src/main/scala/chap04/exer.scala
@@ -97,3 +97,32 @@ object Exer45 {
   def sequence[A](a: List[Option[A]]): Option[List[A]] =
     traverse(a)(identity)
 }
+
+trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(a) => f(a)
+      case Left(e) => Left(e)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(_) => b
+      case _ => this
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      x<-this;
+      y <- b
+    } yield f(x, y)
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
